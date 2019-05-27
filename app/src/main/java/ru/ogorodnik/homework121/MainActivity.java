ce53e40729da36a5c56dac2b9fa5da6b7b56f356
OgorodnikSergey <osv8487@gmail.com>
2019-05-28 01:54:41 +0300

    HomeWork121v2

--- app/src/main/java/ru/ogorodnik/homework121/MainActivity.java
+++ app/src/main/java/ru/ogorodnik/homework121/MainActivity.java
@@ -25,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
         buttonOk.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //String str1 = getString(R.string.text1);
+                   // String str1 = getString(R.string.text1, userNameText.getText(), userMailText.getText());
                    // String str2 = getString(R.string.text2);
                    // exampleText.setText(str1 + " " + userNameText.getText() + " " + str2 + " " + userMailText.getText());
-                    exampleText.setText(getString(R.string.text1)+ " " + userNameText.getText() + " " + getString(R.string.text2)+ " " + userMailText.getText());
+                    exampleText.setText(getString(R.string.text1, userNameText.getText(), userMailText.getText()));
                 }
         });
         // Нажимаем на кнопку ОЧИСТИТЬ
